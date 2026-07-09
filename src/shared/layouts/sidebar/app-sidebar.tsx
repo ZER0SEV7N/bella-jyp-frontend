cc038ad4cd14c6b2ee38bfd120cd4256b163c74e
crodrigoDev <212727257+crodrigoDev@users.noreply.github.com>
2026-07-08 19:15:02 -0500

    feat: avatar section - se creo la seccion de avatar + nombre y rol en el app-header

--- src/shared/layouts/sidebar/app-sidebar.tsx
+++ src/shared/layouts/sidebar/app-sidebar.tsx
@@ -3,7 +3,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import { usePathname } from 'next/navigation'
-import { useState } from 'react'
 import { LogOut } from 'lucide-react'
 import {
   Sidebar,
