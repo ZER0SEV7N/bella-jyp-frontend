d8798a1d3c6ad8900ae9a08359c87a7710cfb426
Zairex-Code <dylanyesidflorez@gmail.com>
2026-07-12 00:27:54 -0500

    test(tareas): configurar vitest e implementar aserciones para ui optimista

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone", // aislar dependencias para Docker
+  
   experimental: {
     serverAction: {
       bodySizeLimit: "2mb",
